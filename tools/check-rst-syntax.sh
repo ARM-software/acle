a6079ef8a09f28b0a587a87a74bd9fbc2746288c
Francesco Petrogalli <francesco.petrogalli@arm.com>
2021-05-21 17:41:36 +0100

    [intrinsics] Intrinsics database and PDF generation for NEON and MVE.

--- tools/check-rst-syntax.sh
+++ tools/check-rst-syntax.sh
@@ -19,3 +19,5 @@ set -ex
 
 rstcheck --ignore-language=c,cpp --report=warning morello/morello.rst
 rstcheck --ignore-language=c,cpp --report=warning main/acle.rst
+rstcheck --ignore-language=c,cpp --report=warning neon_intrinsics/advsimd.rst
+rstcheck --ignore-language=c,cpp --report=warning mve_intrinsics/mve.rst
