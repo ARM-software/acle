48c0bfd25a91e0d81e0b7e681cb9028794050e5a
Valeria Daneva <valeria.daneva@arm.com>
2021-10-13 12:23:22 +0100

    [github.io] Fixing CI build issues

--- tools/pandoc-pdf-setup.sh
+++ tools/pandoc-pdf-setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install pandoc
-sudo apt-get install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
-sudo apt-get install inkscape
+sudo apt-get -y install pandoc
+sudo apt-get -y install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+sudo apt-get -y install inkscape
