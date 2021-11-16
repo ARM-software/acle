e01be2ec0d3966b99c29da66e24a31116e0a6c1f
Valeria Daneva <valeria.daneva@arm.com>
2021-11-16 15:19:21 +0000

    [github.io] Output error message to a file

--- tools/hyperref-check.sh
+++ tools/hyperref-check.sh
@@ -10,7 +10,8 @@ for file in "./main/acle.md" "./morello/morello.md" "./mve_intrinsics/mve.md" ".
 
   if [[ $(wc -l <<< "$broken_refs") -gt 0 ]]; then
     echo "**** WARNING! These section links have been found to not be working: "
-    echo "$broken_refs" | tr " " "\n"
+    echo "$broken_refs" | tr " " "\n" > erroutput.txt
+    cat erroutput.txt
     exit 1
   fi
 done
