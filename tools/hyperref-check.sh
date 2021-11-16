deb6f15f225343caaecefcbe8c79f04f9bf01581
Valeria Daneva <valeria.daneva@arm.com>
2021-11-16 16:50:43 +0000

    [github.io] Attempt at CI fix

--- tools/hyperref-check.sh
+++ tools/hyperref-check.sh
@@ -7,9 +7,8 @@ for file in "./main/acle.md" "./morello/morello.md" "./mve_intrinsics/mve.md" ".
 
   pandoc $file --verbose --fail-if-warnings -o pdfs/tmp.pdf &> erroutput.txt
   cat erroutput.txt | grep -E 'pdfTeX warning \(dest\): name{[^}]+}' | sed -E 's/.*name\{([^}]+)\}.*/\1/' | sort | uniq > list_of_broken_links.txt
-  number_of_broken_refs=$(cat list_of_broken_links.txt | wc -l)
 
-  if [[ "$number_of_broken_refs" -gt 0 ]]; then
+  if [[ $(wc -l <<< list_of_broken_links.txt) -gt 0 ]]; then
       echo "**** WARNING! These section links have been found not to be working: "
       cat list_of_broken_links.txt
       exit 1
