596f3c4ac854ad214234bb5e60a8d75351cf4a91
Valeria Daneva <valeria.daneva@arm.com>
2021-11-16 13:18:54 +0000

    [github.io] Address requested changes

--- tools/hyperref-check.sh
+++ tools/hyperref-check.sh
@@ -5,8 +5,8 @@ set -x
 for file in "./main/acle.md" "./morello/morello.md" "./mve_intrinsics/mve.md" "./neon_intrinsics/advsimd.md"; do
   echo "Checking $file..."
 
-  broken_refs=$(pandoc $file --verbose --fail-if-warnings -o pdfs/tmp.pdf 2>&1 | \
-  grep -E 'pdfTeX warning \(dest\): name{[^}]+}' | sed -E  's/.*name\{([^}]+)\}.*/\1/' | sort | uniq)
+  broken_refs=`pandoc $file --verbose --fail-if-warnings -o pdfs/tmp.pdf 2>&1 | \
+  grep -E 'pdfTeX warning \(dest\): name{[^}]+}' | sed -E  's/.*name\{([^}]+)\}.*/\1/' | sort | uniq`
 
   if [[ $(wc -l <<< "$broken_refs") -gt 0 ]]; then
     echo "**** WARNING! These section links have been found to not be working: "
