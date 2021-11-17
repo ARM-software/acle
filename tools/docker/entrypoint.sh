279d85be76a9fcbb3bc558b511b0d6cbd1b39d9a
Francesco Petrogalli <25690309+fpetrogalli@users.noreply.github.com>
2021-11-17 14:35:50 +0000

    Update tools/docker/entrypoint.sh
    Co-authored-by: ValeriaDaneva <90907783+ValeriaDaneva@users.noreply.github.com>

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -7,7 +7,7 @@ cd /src
 echo $PWD
 ls -al
 
-# Generate the intrinsic specifications and the PDFs. If any of this
+# Generate the intrinsic specifications and the PDFs. If this step fails at any point, stop the docker run.
 # step fails, make the docker run stop.
 /src/tools/generate-intrinsics-specs.sh || exit 1
 /src/tools/generate-pdfs.sh || exit 1
