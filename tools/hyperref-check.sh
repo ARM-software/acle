a3600daa84956bb3b59f14ecfabf22f5cd968c28
Valeria Daneva <valeria.daneva@arm.com>
2021-11-17 18:14:19 +0000

    [github.io] Fix CI output, restructure code to not use files for I/O

--- tools/hyperref-check.sh
+++ tools/hyperref-check.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-set -x
 
 # Extracting all broken hyperref detected by PDFTex
 for file in "./main/acle.md" "./morello/morello.md" "./mve_intrinsics/mve.md" "./neon_intrinsics/advsimd.md"; do
   echo "Checking $file..."
 
-  pandoc $file --verbose --fail-if-warnings -o pdfs/tmp.pdf &> tools/erroutput.txt
-  cat tools/erroutput.txt | grep -E 'pdfTeX warning \(dest\): name{[^}]+}' | sed -E 's/.*name\{([^}]+)\}.*/\1/' | sort | uniq > tools/list_of_broken_links.txt
-  number_of_broken_refs=`cat tools/list_of_broken_links.txt | wc -l`
+  broken_refs=`pandoc $file --verbose --fail-if-warnings -o pdfs/tmp.pdf 2>&1 | \
+               grep -E 'pdfTeX warning \(dest\): name{[^}]+}' | sed -E  's/.*name\{([^}]+)\}.*/\1/' | \
+               sort | uniq | tr " " "\n"`
+  number_of_broken_refs=`echo "$broken_refs" | wc -l`
 
   if [[ $number_of_broken_refs -gt 0 ]]; then
       echo "**** WARNING! These section links have been found not to be working: "
-      cat tools/list_of_broken_links.txt
+      echo "$broken_refs"
       exit 1
   fi
 done
