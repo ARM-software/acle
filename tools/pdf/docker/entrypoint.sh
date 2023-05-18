da2507af64fe019c915dc85ce19c5c79d500e8f8
Victor Campos <victor.campos@arm.com>
2023-05-18 11:37:04 +0100

    Rearrange scripts for PDF generation
    All the files related to PDF generation have been moved into a new
    directory called 'pdf'. Moreover, the script build_with_docker.sh has
    been renamed to build-pdfs.sh because another script will be added to
    build the GitHub Pages, so this name is more specific about what it
    actually does.

--- tools/pdf/docker/entrypoint.sh
+++ tools/pdf/docker/entrypoint.sh
@@ -9,6 +9,7 @@ ls -al
 
 # Generate the intrinsic specifications and the PDFs. If this step
 # fails at any point, stop the docker run.
-/src/tools/hyperref-check.sh || exit 1
-/src/tools/generate-intrinsics-specs.sh || exit 1
-/src/tools/generate-pdfs.sh || exit 1
+/src/tools/pdf/hyperref-check.sh || exit 1
+/src/tools/pdf/generate-intrinsics-specs.sh || exit 1
+/src/tools/pdf/generate-pdfs.sh || exit 1
+rm -rf tmp
