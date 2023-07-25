5b01857eaffc0bd2f8327914f1a168039b4ba92c
Victor Campos <victor.campos@arm.com>
2023-07-25 13:37:28 +0100

    Revert "Rearrange scripts for PDF generation"
    This reverts commit da2507af64fe019c915dc85ce19c5c79d500e8f8.
    
    The output PDFs have a bug where the section numbers are one depth level
    below where they should be.

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -6,8 +6,7 @@
 set -x
 IMAGE_NAME=acle_build
 # Build the image.
-docker build -t $IMAGE_NAME tools/pdf/docker
+docker build -t $IMAGE_NAME tools/docker
 # Run the image, mounting the current folder into the /src folder of
 # the docker image.
-ROOTDIR=$(realpath "$(dirname "$(realpath "$0")")/..")
-docker run --rm -v "$ROOTDIR":/src $IMAGE_NAME
+docker run --rm --mount type=bind,source="$(pwd)",target=/src $IMAGE_NAME
